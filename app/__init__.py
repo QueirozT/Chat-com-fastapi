5582ea796721b53ff819a9f6f66c568a89bd96bc
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-08-07 01:30:49 -0300

    Alterando as rotas do projeto

--- app/__init__.py
+++ app/__init__.py
@@ -3,48 +3,12 @@ from fastapi import (
 )
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import json
+from manager import ws_manager
 
 
 app = FastAPI()
 
 # StartApp: uvicorn app:app --port 8000
-
-
-# Gerenciador de conexões
-class ConnectionManager:
-    def __init__(self):
-        self.connections = []
-        self.users = []
-
-    async def connect(self, websocket, user):
-        await websocket.accept()
-        self.connections.append(websocket)
-        self.users.append(user)
-        await self.broadcast(
-            json.dumps({
-                "action": "message", "user": "","message": f">> {user} << entrou na sala!"
-            })
-        )
-
-    async def broadcast(self, message):
-        if self.connections:
-            for conn in self.connections:
-                await conn.send_text(message)
-                # print(message)
-
-    async def disconnect(self, websocket, user):
-        self.connections.remove(websocket)
-        self.users.remove(user)
-        await self.broadcast(
-            json.dumps({
-                "action": "message", "user": "","message": f">> {user} << Saiu da sala."
-            })
-        )
-
-
-ws_manager = ConnectionManager()
-
 
 # Template
 templates = Jinja2Templates(directory='templates')
@@ -58,11 +22,8 @@ def route(request: Request, response_classe=HTMLResponse):
     )
 
 
-@app.websocket('/{user}')
-async def push_endpoint(
-        websocket: WebSocket,
-        user: str,
-):
+@app.websocket('/chat')
+async def push_endpoint(websocket: WebSocket):
     user = user.replace('{', '').replace('}', '')
     await ws_manager.connect(websocket, user)
     try:
